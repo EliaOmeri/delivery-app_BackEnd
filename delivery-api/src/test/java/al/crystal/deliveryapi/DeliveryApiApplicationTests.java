680955595cc048d81d843ea0059610afc6fbf88a
IrenaShahini <irenashahini.ee@gmail.com>
2023-02-28 12:25:04 +0100

    Backedend Application

---
+++ delivery-api/src/test/java/al/crystal/deliveryapi/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package al.crystal.deliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
