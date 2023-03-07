7e93935e61f40d7628f463c822c6ace6674cf586
Griselda2022 <griselda.muci@crystal-system.eu>
2023-03-07 10:20:14 +0100

    Configured and implemented in API application to use Liquibase.

--- delivery-api/src/main/java/al/crystal/deliveryapi/DeliveryApiApplication.java
+++ delivery-api/src/main/java/al/crystal/deliveryapi/DeliveryApiApplication.java
@@ -3,11 +3,14 @@ package al.crystal.deliveryapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class DeliveryApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeliveryApiApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryApiApplication.class, args);
+    }
 
 }
